eb04e3c7115b7080e2ccf3d54963599d5fe92893
Archaeologist03 <omniiknight@hotmail.com>
2020-06-06 21:38:33 +0200

    Configured CircleCI and Netlifly. Deploying tested.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div className='App'>Yo there, check ma cricleCI + netlify.</div>;
+  return <div className='App'>Yo there, check ma cricleCI + netlify. For real.</div>;
 }
 
 export default App;

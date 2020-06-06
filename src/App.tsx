55745c1737c1ac18194a1ec6d91eda5032b3a35c
Archaeologist03 <omniiknight@hotmail.com>
2020-06-06 19:13:58 +0200

    Checking Travis - test/build/deploy.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div className='App'>Yo there</div>;
+  return <div className='App'>Yo there, check ma travis.</div>;
 }
 
 export default App;
